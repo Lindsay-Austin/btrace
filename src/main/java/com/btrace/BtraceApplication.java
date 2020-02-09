5e55bb135be4aba87e93cb084ce40a124a0c5900
zj <382074290@qq.com>
2020-02-09 13:49:40 +0800

    首次提交

---
+++ src/main/java/com/btrace/BtraceApplication.java
@@ -0,0 +1,13 @@
+package com.btrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/btrace/BtraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
